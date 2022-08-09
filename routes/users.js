66fe450d683958fb07a8a634e3a2602f6e313af5
Charles Peace <charlespeace@hotmail.com>
2022-08-09 12:51:55 -0700

    add and test create route

--- routes/users.js
+++ routes/users.js
@@ -18,8 +18,18 @@ router.get('/:id', (req, res) => {
 })
 
 // Create one
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        createdDate: req.body.createdDate,
+        email: req.body.email
+    })
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 
 // Update one
