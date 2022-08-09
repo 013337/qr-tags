342f775bf27dd2e7843930fe4fea6ca37a1cf28b
Charles Peace <charlespeace@hotmail.com>
2022-08-09 13:18:37 -0700

    add getUser middleware into users.js and add/test delete route

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
 })
 
 // Read one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', getUser, (req, res) => {
+    res.json(res.user)
 })
 
 // Create one
@@ -33,13 +33,32 @@ router.post('/', async (req, res) => {
 })
 
 // Update one
-router.patch('/', (req, res) => {
+router.patch('/', getUser, async (req, res) => {
 
 })
 
 // Delete one
-router.delete('/', (req, res) => {
-
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove()
+        res.json({ message: 'Deleted User' })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 })
+
+async function getUser(req, res, next) {
+    let user
+    try {
+        user = await User.findById(req.params.id)
+        if (user == null) {
+            return res.status(404).json({ message: 'Cannot find user' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+    res.user = user
+    next()
+}
 
 module.exports = router
